e1da74db116af9c808360012713a537fdb84b7f5
osnat ben ari <osnat122ster@gmail.com>
2020-08-26 13:40:14 +0300

    update

--- amazon.js
+++ amazon.js
@@ -1,15 +1,15 @@
 const buyBtns = document.querySelectorAll('button.buy');
-const cart = document.querySelector('.cartItems')
-let items = 0
+const cart = document.querySelector('.cartItems');
+let items = 0;
 
 
 function addToCart() {
-    items += 1
+    items += 1;
     cart.textContent = `${items}`;
 }
 
 
 buyBtns.forEach( function(buyButton) {
-    console.log(buyButton)
+    
     buyButton.addEventListener('click', addToCart);
 })
